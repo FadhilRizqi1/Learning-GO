3da778479e51697e6a0b199cf38e506a7433eabb
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-05-29 23:13:22 +0700

    Add array.go to demonstrate array usage in Go

---
+++ Learning-GO/array.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nama [5]string
+
+	nama[0] = "Fadhil"
+	nama[1] = "Rizqi"
+	nama[2] = "Ahmad"
+	nama[3] = "Falzi"
+	nama[4] = "MAFALQI"
+
+	fmt.Println(nama[4])
+}
+
