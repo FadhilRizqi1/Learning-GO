472b031651342a30955c46f2c370ba83ed6b92e3
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-05-30 23:46:38 +0700

    Add map.go to demonstrate map usage in Go

---
+++ Learning-GO/map.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	orang := map[string]string{
+		"nama":    "Fadhil",
+		"alamat":  "Palembang",
+		"pekerjaan": "Programmer/IT Support",
+		"umur":    "19",
+	}
+
+	fmt.Println(orang)
+}
