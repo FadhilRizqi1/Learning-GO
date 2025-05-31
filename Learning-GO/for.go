26535105af7075ac34a109321e92e1edb5408dd2
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-05-31 19:15:31 +0700

    Add iteration logic to for.go for even and odd number printing

--- Learning-GO/for.go
+++ Learning-GO/for.go
@@ -6,11 +6,20 @@ func main() {
 
 	var (
 		perulangan int = 0
+		i int
 	)
 
 	for perulangan <= 10 {
 			fmt.Println ("Perulangan ke-", perulangan)
 			perulangan++
+	}
+
+	for i = 0; i <= 10; i++ {
+		if i % 2 == 0 {
+			fmt.Println("Bilangan genap:", i)
+		} else {
+			fmt.Println("Bilangan ganjil:", i)
+		}
 	}
 
 }
