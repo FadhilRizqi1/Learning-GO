c24f0abd28ea048a3b4920db078be646d72d6847
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-05-31 18:48:15 +0700

    Add input validation for name length in grading system

--- Learning-GO/if.go
+++ Learning-GO/if.go
@@ -7,10 +7,20 @@ func main() {
 	var (
 	nilai int
 	akreditasi string
+	nama string
+	length int
 	)
 
-	fmt.Print("Masukkan nilai: ")
+	fmt.Print("Masukkan nama: ")
+	fmt.Scan(&nama)
 
+	if length = len(nama); length <= 3 {
+		fmt.Println("Nama harus lebih dari 3 karakter")
+		return
+	}
+	
+
+	fmt.Print("Masukkan nilai: ")
 	fmt.Scan(&nilai)
 
 	if nilai >= 90 {
@@ -26,5 +36,7 @@ func main() {
 	}
 
 	fmt.Println("Nilai Anda:", akreditasi)
+
+
 
 }
