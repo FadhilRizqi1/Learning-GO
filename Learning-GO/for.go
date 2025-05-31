02e4a0e8b49838bf80bf398e190c4da1adf9b378
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-05-31 19:13:21 +0700

    Add initial implementation of looping structure in for.go

---
+++ Learning-GO/for.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var (
+		perulangan int = 0
+	)
+
+	for perulangan <= 10 {
+			fmt.Println ("Perulangan ke-", perulangan)
+			perulangan++
+	}
+
+}
