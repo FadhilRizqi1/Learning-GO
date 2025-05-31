0e673950856304a23d54efc8efa6e93eec8780aa
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-05-31 19:04:57 +0700

    Add grading logic to switch.go for accreditation based on nilai

--- Learning-GO/switch.go
+++ Learning-GO/switch.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	
 	var (
+		nilai int
+		akreditasi string
 		nama string
 		length int
 	)
@@ -19,5 +21,23 @@ func main() {
 	case false:
 		fmt.Println("Nama valid")
 	}
+	
+	fmt.Println("Masukkan Nilai:")
+	fmt.Scan(&nilai)
+
+	switch nilai {
+		case 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100:
+			akreditasi = "A"
+		case 80, 81, 82, 83, 84, 85, 86, 87, 88, 89:
+			akreditasi = "B"
+		case 70, 71, 72, 73, 74, 75, 76, 77, 78, 79:
+			akreditasi = "C"
+		case 60, 61, 62, 63, 64, 65, 66, 67, 68, 69:
+			akreditasi = "D"
+		default:
+			akreditasi = "E"
+	}
+
+	fmt.Println("Nilai Anda:", akreditasi)
 
 }
