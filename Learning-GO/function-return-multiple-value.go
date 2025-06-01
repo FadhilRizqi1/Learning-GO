077f2a29489238b3d87e9ed5c1040828d2cd3fe6
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-06-01 20:30:42 +0700

    feat: update returnMultiple function to include middle name and adjust main function accordingly

--- Learning-GO/function-return-multiple-value.go
+++ Learning-GO/function-return-multiple-value.go
@@ -2,20 +2,23 @@ package main
 
 import "fmt"
 
-func returnMultiple(firstName string, lastName string) (string, string) {
-	return "Halo", firstName + " " + lastName
-}
+func returnMultiple(firstName string, middleName string, lastName string) (string, string, string) {
+	return "Halo", firstName + " " + middleName + " " + lastName , "Selamat datang"
+} 
 
 func main() {
-
-	var firstName, lastName string
+	
+	var firstName, middleName, lastName string
 
 	fmt.Println("Masukkan nama depan anda:")
 	fmt.Scan(&firstName)
 
+	fmt.Println("Masukkan nama tengah anda:")
+	fmt.Scan(&middleName)
+
 	fmt.Println("Masukkan nama belakang anda:")
 	fmt.Scan(&lastName)
 
-	fmt.Println(returnMultiple(firstName, lastName))
+	fmt.Println(returnMultiple(firstName, middleName, lastName))
 
 }
