9534cc5fe482aa39000d69b40a896ff9c29a0fd5
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-06-02 22:52:33 +0700

    feat: add user registration functionality with blacklist check

---
+++ Learning-GO/function-anonymous.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type Blacklist = func(name string) bool
+var name string
+
+func registerUser(name string, blacklist Blacklist) {
+	if blacklist(name) {
+		fmt.Println("User " + name + " is blacklisted and cannot be registered.")
+	} else {
+		fmt.Println("User " + name + " has been successfully registered.")
+	}
+}
+
+func main(){
+	blacklist := func(name string) bool {
+		return name == "admin" || name == "root" 
+	}
+
+	registerUser("Fadhil", blacklist)
+	registerUser("admin", blacklist)
+}
