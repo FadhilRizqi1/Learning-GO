93c4910118541ea6a23f6344023df862123d0634
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-06-03 22:27:08 +0700

    feat: add initial comment examples

---
+++ Learning-GO/comment.go
@@ -0,0 +1,11 @@
+package main
+
+//Ini adalah contoh komen dengan satu baris
+// Komen ini menjelaskan bahwa program ini tidak melakukan apa-apa
+
+func main() {
+	/**
+	Ini adalah contoh komen dengan beberapa baris
+	Ini bisa digunakan untuk menjelaskan kode yang lebih kompleks
+	*/
+}
