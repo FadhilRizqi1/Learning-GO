293f17370b5414c8109af8c06cc1d4631ae0d14c
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-06-04 22:11:26 +0700

    feat: Update example of pointer as operator

--- Learning-GO/pointer.go
+++ Learning-GO/pointer.go
@@ -8,10 +8,14 @@ type Adress struct {
 
 func main() {
 	alamat1 := Adress{"Palembang", "Sumatera Selatan", "Indonesia"}
-	alamat2 := alamat1
+	alamat2 := &alamat1
+	alamat3 := &alamat1
 
-	alamat2.kota = "Jakarta"
+    alamat2.kota = "Jakarta"
+	
+	*alamat2 = Adress{"Bandung", "Jawa Barat", "Indonesia"}
 
 	fmt.Println(alamat2)
 	fmt.Println(alamat1)
+	fmt.Println(alamat3)
 }
