d0a47dc1b885ddc1b33abfce2fb1a069b7273606
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-06-04 14:15:58 +0700

    feat: enhance callName function to handle empty string input

--- Learning-GO/nil.go
+++ Learning-GO/nil.go
@@ -2,7 +2,23 @@ package main
 
 import "fmt"
 
+func callName(name string) map[string]string {
+	if name == " " {
+		return nil
+	} else {
+		return map[string]string{
+			"nama": name,
+		}
+	}
+}
+
 func main() {
 	var contohNil map[string]string = nil
 	fmt.Println(contohNil)
+
+	var filled = callName("Fadhil")
+	fmt.Println(filled)
+
+	var empty = callName(" ")
+	fmt.Println(empty)
 }
