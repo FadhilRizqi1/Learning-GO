26f141145751d4ae420f9e8cf60c955d58037dc6
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-06-04 15:04:46 +0700

    feat: update random function to return boolean and add type assertion handling in main

--- Learning-GO/type-assertions.go
+++ Learning-GO/type-assertions.go
@@ -3,11 +3,22 @@ package main
 import "fmt"
 
 func random() interface{} {
-	return 1
+	return true
 }
 
 func main() {
 	var result interface{} = random()
-	var resultInt = result.(int)
-	fmt.Println("Hasil konversi:", resultInt)
-}
+	// var resultInt = result.(int)
+	// fmt.Println("Hasil konversi:", resultInt)
+
+	switch value := result.(type) {
+	case int:
+		fmt.Println("Hasil konversi:", value, "adalah int")
+	case string:
+		fmt.Println("Hasil konversi:", value, "adalah string")
+	case bool:
+		fmt.Println("Hasil konversi:", value, "adalah bool")
+	default:
+		fmt.Println("Tipe data tidak dikenali")
+	}
+}	
