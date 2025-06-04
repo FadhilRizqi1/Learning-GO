1e11786377926bd4b10e87dab842bb892cc37c3a
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-06-04 13:40:34 +0700

    feat: implement Orang struct and tampilkanNama function for displaying personal information

---
+++ Learning-GO/interface.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type nama interface {
+	tampilkanNama() string
+}
+
+func tampilkanNama(orang nama) {
+	fmt.Println(orang.tampilkanNama())
+}
+
+type Orang struct {
+	namaDepan, namaBelakang string
+	umur int
+}
+
+func (orang Orang) tampilkanNama() string {
+	return "Nama: " + orang.namaDepan + " " + orang.namaBelakang + ", Umur: " + fmt.Sprint(orang.umur)
+}
+
+func main() {
+	var Fadhil Orang
+
+	Fadhil.namaDepan = "Fadhil"
+	Fadhil.namaBelakang = "Rizqi"
+	Fadhil.umur = 19
+
+	tampilkanNama(Fadhil)
+}
