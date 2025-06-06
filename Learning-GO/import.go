823ff6610b33e5d4362fe36aacbc543dbc3100f2
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-06-06 23:06:46 +0700

    feat: update main function to demonstrate access to exported variable

--- Learning-GO/import.go
+++ Learning-GO/import.go
@@ -3,9 +3,13 @@ package main
 import (
 	"Learning-GO/helper"
 	"Learning-GO/other"
+	"fmt"
 )
 
 func main() {
 	helper.SayName("Fadhil")
 	other.SayName("Rizqi")
+
+	// fmt.Print(helper.unexported) // error: helper.version is not exported
+	fmt.Print(helper.Exported) // exported variable can be accessed
 }
