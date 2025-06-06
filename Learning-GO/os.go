2bd377a1c82c4662eac6cf5446e108f1f1a3d714
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-06-06 23:29:19 +0700

    feat: add hostname retrieval and error handling in main function

--- Learning-GO/os.go
+++ Learning-GO/os.go
@@ -9,4 +9,11 @@ func main() {
 	arg := os.Args
 	fmt.Println("Argument : ")
 	fmt.Println(arg)
+
+	name, error := os.Hostname()
+	if error != nil {
+		fmt.Println("Error : ", error)
+	} else {
+		fmt.Println("Hostname : ", name)
+	}
 }
