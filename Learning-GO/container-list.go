5301e6cb529f627c3a641f22791f067883b3e85c
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-06-07 21:29:14 +0700

    feat: enhance container list example with additional traversal outputs

--- Learning-GO/container-list.go
+++ Learning-GO/container-list.go
@@ -14,4 +14,12 @@ func main() {
 
 	fmt.Println(data.Front().Value)
 	fmt.Println(data.Back().Value)
+
+	for list := data.Front(); list != nil; list = list.Next() {
+		fmt.Println(list.Value)
+	}
+
+	for list := data.Back(); list != nil; list = list.Prev() {
+		fmt.Println(list.Value)
+	}
 }
