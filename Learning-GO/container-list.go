35e985b44d754254e5fb2b6abb43efe7862db0cb
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-06-07 21:30:22 +0700

    docs: add comments to clarify traversal order in container list example

--- Learning-GO/container-list.go
+++ Learning-GO/container-list.go
@@ -15,10 +15,12 @@ func main() {
 	fmt.Println(data.Front().Value)
 	fmt.Println(data.Back().Value)
 
+	// front to back
 	for list := data.Front(); list != nil; list = list.Next() {
 		fmt.Println(list.Value)
 	}
 
+	// back to front
 	for list := data.Back(); list != nil; list = list.Prev() {
 		fmt.Println(list.Value)
 	}
