4a7eacf6788775b722a00747e69d2ae86414e5f1
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-06-08 20:20:53 +0700

    feat: add TrimSpace and ReplaceAll examples in strings.go

--- Learning-GO/strings.go
+++ Learning-GO/strings.go
@@ -14,4 +14,7 @@ func main() {
 	fmt.Println(strings.ToLower("Ahmad Fadhil Rizqi"))
 	fmt.Println(strings.ToUpper("Ahmad Fadhil Rizqi"))
 	fmt.Println(strings.Trim("   Ahmad Fadhil Rizqi   ", " "))
+	fmt.Println(strings.TrimSpace("   Ahmad Fadhil Rizqi   "))
+	fmt.Println(strings.ReplaceAll("Ahmad Fadhil Rizqi", "Fadhil", "Fadhil Rizqi"))
+
 }
