07ed0a806a8fc0159269ab7ac5b808adfe258c6f
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-06-10 16:34:35 +0700

    feat: add JSON tag to sample struct and implement validation logic

--- Learning-GO/reflect.go
+++ Learning-GO/reflect.go
@@ -6,7 +6,36 @@ import (
 )
 
 type sample struct {
-	Nama string `required:"true" max:"10" json:"nama"`
+	Nama string `required:"true" max:"10"`
+}
+
+type sample2 struct {
+	Nama string
+	Umur int
+}
+
+func validate(data interface{}) bool {
+	t := reflect.TypeOf(data)
+	v := reflect.ValueOf(data)
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		tag := field.Tag.Get("required")
+		value := v.Field(i)
+
+		if tag == "true" && value.String() == "" {
+			return false
+		}
+
+		maxTag := field.Tag.Get("max")
+		if maxTag != "" {
+			maxLength := 0
+			fmt.Sscanf(maxTag, "%d", &maxLength)
+			if len(value.String()) > maxLength {
+				return false
+			}
+		}
+	}
+	return true
 }
 
 func main() {
@@ -19,5 +48,9 @@ func main() {
 	fmt.Println("Tag:", SampleType.Field(0).Tag)
 	fmt.Println("Tag JSON:", SampleType.Field(0).Tag.Get("required"))
 	fmt.Println("Tag XML:", SampleType.Field(0).Tag.Get("max"))
-	fmt.Println("Tag YAML:", SampleType.Field(0).Tag.Get("json"))
+
+	sample.Nama = ""
+	fmt.Println("Valid:", validate(sample))
+
+	fmt.Println("Valid:", validate(sample2{Nama: "Fadhil", Umur: 19}))
 }
