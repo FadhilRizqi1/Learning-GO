725758ed9deccad576064cbf7e1e8e188e32f106
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-06-10 16:27:16 +0700

    feat: add struct field tags for validation and JSON serialization

--- Learning-GO/reflect.go
+++ Learning-GO/reflect.go
@@ -6,7 +6,7 @@ import (
 )
 
 type sample struct {
-	Nama string
+	Nama string `required:"true" max:"10" json:"nama"`
 }
 
 func main() {
@@ -15,4 +15,9 @@ func main() {
 	var SampleType = reflect.TypeOf(sample)
 	fmt.Println("Type:", SampleType.NumField())
 	fmt.Println("Name:", SampleType.Field(0).Name)
+
+	fmt.Println("Tag:", SampleType.Field(0).Tag)
+	fmt.Println("Tag JSON:", SampleType.Field(0).Tag.Get("required"))
+	fmt.Println("Tag XML:", SampleType.Field(0).Tag.Get("max"))
+	fmt.Println("Tag YAML:", SampleType.Field(0).Tag.Get("json"))
 }
