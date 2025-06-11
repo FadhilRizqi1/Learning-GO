6b4ef5dd2ac2821916209d6f7fcbf3d3867e1306
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-06-11 21:16:53 +0700

    feat: add initial implementation of main function to greet user

---
+++ Learning-Go-Package-Library/fmt.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstName := "Ahmad"
+	middleName := "Fadhil"
+	lastName := "Rizqi"
+
+	fmt.Printf("Hello, my name is '%s %s %s'\n", firstName, middleName, lastName)
+}
