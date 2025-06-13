5dd1a983437001640d121cca7aa649259b2f6aba
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-06-13 20:53:02 +0700

    feat: add slices.go for demonstrating slice max and min functionality

---
+++ Learning-Go-Package-Library/slices.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	name := []string{"Ahmad", "Fadhil", "Rizqi", "MAFALQI", "Falqi"}
+	value := []int{100, 90, 80, 70, 60}
+
+
+	fmt.Println(slices.Max(value))
+	fmt.Println(slices.Min(name))
+}
