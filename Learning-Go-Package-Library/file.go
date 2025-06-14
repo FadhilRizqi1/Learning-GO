d347c60c4e7d58e8ded06c45d1a2074e0faaa950
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-06-14 19:37:59 +0700

    feat: enhance file.go with readFile function for reading file contents

--- Learning-Go-Package-Library/file.go
+++ Learning-Go-Package-Library/file.go
@@ -1,6 +1,10 @@
 package main
 
-import "os"
+import (
+	"bufio"
+	"io"
+	"os"
+)
 
 func createNewFile(filename string, message string) error {
 	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
@@ -17,11 +21,39 @@ func createNewFile(filename string, message string) error {
 	return nil
 }
 
+func readFile(filename string) (string, error) {
+	file, err := os.OpenFile(filename, os.O_RDONLY, 0666)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	var message string
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return "", err
+		}
+		message += string(line) + "\n"
+	}
+	return message, nil
+}
+
+
 func main () {
-	err := createNewFile("example.txt", "Halo!")
+	err := createNewFile("example.txt", "Ini adalah contoh file.\n")
 	if err != nil {
 		panic(err)
 	}
 
-	println("File created successfully")
+	result, err := readFile("example.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	println(result)
 }
